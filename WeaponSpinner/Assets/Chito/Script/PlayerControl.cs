933ccd95fe9234fbb0a867e7cb6e59b150e36936
Unknown <kimjisoo690@gmail.com>
2018-03-28 19:11:30 +0900

    Make Prototype Ragdoll Character
    프로토타입의 레그톨 캐릭터 생성.
    wasd 이동, space 점프 / 팔의 타원구가 무기입니다.

---
+++ WeaponSpinner/Assets/Chito/Script/PlayerControl.cs
@@ -0,0 +1,85 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControl : MonoBehaviour {
+
+    public float move_speed = 10;
+    public float look_speed = 1;
+
+    public Rigidbody p_rigidbody;
+
+    public Camera p_camera;
+
+    Vector3 targetPosition;
+    Vector3 lookPosition;
+
+    Animator anim;
+
+
+    private void Awake()
+    {
+        p_rigidbody = GetComponent<Rigidbody>();
+        anim = GetComponent<Animator>();
+    }
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        Walk();
+        Look();
+    }
+
+    void Look()
+    {
+        lookPosition = Camera.main.transform.forward;
+        lookPosition.y = 0;
+        transform.rotation = Quaternion.Lerp(transform.rotation,
+            Quaternion.LookRotation(lookPosition), Time.fixedDeltaTime * look_speed);
+    }
+
+    void Walk()
+    {
+        if (Input.anyKey)
+        {
+            targetPosition = Vector3.zero;
+            if (Input.GetKey(KeyCode.W))
+            {
+                targetPosition += Camera.main.transform.forward;
+                anim.SetBool("isWalking", true);
+            }
+            if (Input.GetKey(KeyCode.S))
+            {
+                targetPosition += Camera.main.transform.forward * -1;
+                anim.SetBool("isWalking", true);
+            }
+            if (Input.GetKey(KeyCode.A))
+            {
+                targetPosition += Camera.main.transform.right * -1;
+                anim.SetBool("isWalking", true);
+            }
+            if (Input.GetKey(KeyCode.D))
+            {
+                targetPosition += Camera.main.transform.right;
+                anim.SetBool("isWalking", true);
+            }
+
+            targetPosition.y = 0;
+            //transform.position += targetPosition * move_speed * Time.deltaTime;
+            p_rigidbody.AddForce(targetPosition * move_speed, ForceMode.Impulse);
+
+            if (Input.GetKey(KeyCode.Space))
+            {
+                p_rigidbody.AddForce(Vector3.up * 25, ForceMode.Impulse);
+            }
+        }
+        else
+        {
+            anim.SetBool("isWalking", false);
+        }
+    }
+}
