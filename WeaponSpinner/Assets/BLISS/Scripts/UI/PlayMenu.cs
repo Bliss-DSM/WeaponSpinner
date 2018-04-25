59469a9e2eb0a82f9bd5edb66dc5330c52351f58
Kyungwook Cho <amisay5256@naver.com>
2018-04-25 20:26:13 +0900

    Add UI Script

---
+++ WeaponSpinner/Assets/BLISS/Scripts/UI/PlayMenu.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayMenu : Menu<PlayMenu>
+{
+	public Button buttonPause;
+	public Button buttonAttack;
+	public Button buttonDash;
+	public Joystick joystick;
+
+	protected override void Awake()
+	{
+		base.Awake();
+		buttonPause.onClick.AddListener(() => { PauseMenu.Open();});
+	}
+}
